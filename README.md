62cfbc133a985920c1df22c5409a84610609b7c7
naslyazghi <nas.lyazghi@knights.ucf.edu>
2020-05-23 19:13:23 -0400

    added AddContact.php file

---
+++ README.md
@@ -0,0 +1,15 @@
+# Cosmic Contact
+
+## Description
+This is a small project for UCF COP-4331, Processes of Object Oriented Software Development. It's a contact manager where you can create an account and store contact information to bring to you everywhere.
+
+## Technology used
+TBA
+
+## Credits
+* Anass Lyazghi
+* Austin Traub
+* Jeffrey Ramos
+* Matthew Bautista
+* Moises David Lugo
+* Ryan Gordon
---
+++ api/AddContact.php
@@ -0,0 +1,54 @@
+<?php 
+    // 1 - Request Info
+    $inData = getRequestInfo();
+
+    // 2 - Extract the contact info
+    $userID = $inData["userId"];
+    $firstname = $inData["firstname"];
+    $lastname = $inData["lastname"];
+    $email = $inData["email"];
+    $phone = $inData["phone"];
+
+    // 3 - Create a connection to the database (localhost, username, password, database name)
+    $conn = new mysqli("localhost", "nas", "sx1qJa3kO8A#", "cosmiccontact");
+
+    // 4 - Check Connection
+    // Connection Failed
+    if ($conn->connect_error)
+    {
+        header('Content-type: application/json');
+		echo '{"error":"' . $conn->connect_error . '"}';
+    }
+    // Connection is successful
+    else
+    {
+        // Generate SQL code to add the contact to the database
+        $sql = "INSERT INTO Contacts(first_name, last_name, email, phone, user_id) 
+                VALUES ('" . $firstname . "', '" . $lastname . "', '" . $email . "', '" . $phone . "', " . $userID . ")";
+                        
+    }
+
+    // 5 -Check if contact was added
+    // Contact was not added, send an error
+    if ($conn->query($sql) != TRUE)
+    {
+        header('Content-type: application/json');
+        echo '{"Error Message":"' . $conn->error . '"}';
+        $conn->close();
+    }
+    // Contact was added successfully 
+    else
+    {
+        header('Content-type: application/json');
+        //echo '{"Message":"Contact successfully added!!"}';
+        echo '{"Message":  "'. $firstname .'  '.$lastname . ' was successfully added!!"}';
+        $conn->close();
+    }
+
+    // Request the info
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+?>
